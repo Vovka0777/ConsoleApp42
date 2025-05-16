068af964beafe423a978521e4f9e96f0e82ca515
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-05-16 12:08:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            Handler1 handler1 = new Handler1();
+            Handler2 handler2 = new Handler2();
+
+            counter.onCount += handler1.Message;
+            counter.onCount += handler2.Message;
+            counter.onCount += counter.Message;
+            counter.Count();
+            Console.Read();
+        }
+    }
+}
